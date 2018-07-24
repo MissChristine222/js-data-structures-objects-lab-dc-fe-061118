40d14ed246f90748738f9ee3b49c423995ce5357
Christine Patton <christine@pattoncreative.com>
2018-07-24 23:51:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const driver = { name: 'Sam'}
 
 function updateDriverWithKeyAndValue(driver, key, value) {
-  const driverAddress = {...driver};
-  driver
+  const driverWithAddress = {...driver};
+  driverWithAddress
   driver.assign({driver}, {address: '11 Broadway'})
 }
