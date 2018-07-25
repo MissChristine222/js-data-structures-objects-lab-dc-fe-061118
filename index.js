bc5eab651137f392b27606f0709ceef22428b8e9
Christine Patton <christine@pattoncreative.com>
2018-07-25 00:20:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
 
 function deleteFromDriverByKey(driver, key) {
   const noDriver = { ...obj };
-  delete noDriver.name 
+  delete noDriver.name;
+  return noDriver;
   return driver;
 } 
